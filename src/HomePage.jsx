8cb83d1197cad706d6756d7085a5615ba0494526
Kartik Kittad <99243266+Kartikkittad@users.noreply.github.com>
2023-10-24 20:00:02 +0530

    Add files via upload

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,6 @@
 import './HomePage.css'
 import Nav from './navbar'
-
+import { Link } from 'react-scroll';
 
 function HomePage() {
 
@@ -15,7 +15,7 @@ function HomePage() {
                     <div className="container">
                         <h3 className="name">Hey, I'm Kartik Kittad</h3>
                         <h1 className="desc">Fullstack <br />Web Developer <br /> And Programmer</h1>
-                        <button className="btn1">Get in touch</button>
+                        <Link to="contacts" spy={true} offset={50} smooth={true} duration={200} className="btn1">Get in touch</Link>
                     </div>
                 </section>
 

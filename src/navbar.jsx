3d142af10b4eba67d9fd5d77b8cf2fb0d729d79e
Kartik Kittad <99243266+Kartikkittad@users.noreply.github.com>
2023-10-27 10:57:26 +0530

    Add files via upload

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,33 +1,24 @@
-
 import './navbar.css'
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
 
-
 function nav() {
-
-
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <div >
-            <nav className="nav fixed-top" >
-
-                <ul>
-                    {/* <li>
-                        <a href="/home" className="home" id="home">Home</a>
-                    </li>
+            <nav className="nav fixed-top">
+                <div className="menu" onClick={() => {
+                    setMenuOpen(!menuOpen)
+                }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className={`ul ${menuOpen ? "open" : ""}`}>
                     <li>
-                        <a href="/about" className="about" id="about">About</a>
-                    </li>
-                    <li>
-                        <a href="/projects" id="projects">Projects</a>
-                    </li>
-                    <li>
-                        <a href="/contact" id="contact">Contact</a>
-                    </li> */}
-                    <li>
-                        <Link to="home" spy={true} offset={-50} smooth={true} duration={200} className="a">Home</Link>
+                        <Link to="home" spy={true} offset={-300} smooth={true} duration={200} className="a">Home</Link>
                     </li>
                     <li>
                         <Link to="about" spy={true} offset={-50} smooth={true} duration={200} className="a">About</Link>
